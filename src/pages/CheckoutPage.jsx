e0eae77cbd4cc0f692915f313624dd375a93a3c4
Luis Tiago <luistiago.andrighetto@gmail.com>
2026-06-19 13:16:23 -0300

    fix

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -739,12 +739,18 @@ export function CheckoutPage() {
     <div className="wrap">
       <header>
         <a href="#" className="logo-custom" style={{ textDecoration: 'none' }}>
-          <svg className="logo-icon-brand" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M6 12h4m-2-2v4M15 13h.01M18 11h.01M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z" />
-          </svg>
-          <div className="logo-label">
-            {storeInfo?.checkout_logo_label || <>DIGITAL STORE <span>GAMES</span></>}
-          </div>
+          {storeInfo?.checkout_logo_url ? (
+            <img src={storeInfo.checkout_logo_url} alt={storeInfo?.checkout_logo_label || "Logo"} style={{ maxHeight: '40px', objectFit: 'contain' }} />
+          ) : (
+            <>
+              <svg className="logo-icon-brand" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M6 12h4m-2-2v4M15 13h.01M18 11h.01M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z" />
+              </svg>
+              <div className="logo-label">
+                {storeInfo?.checkout_logo_label || <>DIGITAL STORE <span>GAMES</span></>}
+              </div>
+            </>
+          )}
         </a>
         <div className="header-secure">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
